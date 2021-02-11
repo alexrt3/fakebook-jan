9b08ccfc94ea3142f8c24af91332b5ddff34e8a9
derekcodingtemple <derekh@codingtemple.com>
2021-02-11 11:58:02 -0600

    first commit

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
